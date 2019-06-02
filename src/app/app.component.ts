ae8423a167fd14f8e72664b7a6d5c524dd1ab0a6
Leocaliban <leocaliban@gmail.com>
2019-06-02 02:35:56 -0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lol-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'lol-academy-br';
+}
